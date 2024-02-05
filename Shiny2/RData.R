18452e099513e64a66bcadeb7ea3790049783f60
Dominik Boehler <dominik.boehler@gmx.net>
2024-02-05 13:41:16 +0100

    fixed error with data import

---
+++ Shiny2/RData.R
@@ -0,0 +1,15 @@
+pacman::p_load(
+  rio,
+  here,
+  dplyr,
+  epikit,
+  forcats,
+  readr,
+  styler,
+  tidyverse,
+  janitor,
+  shiny,
+  ggplot
+)
+
+alcohol_deaths <- read_csv("alcohol_deaths.csv")
